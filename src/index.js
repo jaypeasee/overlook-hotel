5019840034fb2aae99b4e5723a55bbbeb09a1d72
JP Carey <jpcarey4@gmail.com>
2020-11-04 10:06:27 -0700

    validate users on login, begin fetching apis

--- src/index.js
+++ src/index.js
@@ -7,6 +7,71 @@ import Hotel from './Hotel';
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+const loginForm = document.querySelector('.login-form');
+
+let currentUser;
+let currentGuest;
+let manager;
+let hotel;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+window.addEventListener("load", getApiData)
+loginForm.addEventListener("click", handleFormClick);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function getApiData() {
+  let bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+    .then(response => response.json())
+    .then(data => data.bookings)
+    // .then(error) => console.log(error.message)
+  let userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+    .then(response => response.json())
+    .then(data => data.users)
+    // .catch(error) => console.log(error.message)
+  let roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+    .then(response => response.json())
+    .then(data => data.rooms)
+    // .catch(error) => console.log(error.message)
+}
+
+function handleFormClick(event) {
+  const usernameInput = document.querySelector('.login-username-input');
+  const passwordInput = document.querySelector('.login-password-input');
+  if (event.target.className === "login-button"
+  && usernameInput.value !== ""
+  && passwordInput.value !== "") {
+    createUser(usernameInput.value, passwordInput.value);
+  }
+}
+
+function createUser(enteredUsername, enteredPassword) {
+  currentUser = new User(enteredUsername);
+  const userType = currentUser.validateUser(enteredPassword);
+  if (userType === "guest") {
+    createGuest(enteredUsername)
+  } else if (userType === "manager") {
+    createManager(enteredUsername)
+  } else {
+    displayLoginError(userType)
+  }
+}
+
+function createGuest(enteredUsername) {
+  console.log("you created a guest!");
+}
+
+function createManager(enteredUsername) {
+  console.log("you created a manager!");
+}
+
+function displayLoginError(errorMessage) {
+  const messageBlock =
+  `<h4 class="error-message">${errorMessage}</h4>`
+  loginForm.insertAdjacentHTML('beforeend', messageBlock);
+}
+
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
