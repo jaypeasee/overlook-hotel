5d3a736987fdf682cdc71086671c3b7b704be951
JP Carey <jpcarey4@gmail.com>
2020-11-07 14:41:32 -0700

    Create chai spy tests for posting bookings to the api

--- test/api-data-test.js
+++ test/api-data-test.js
@@ -7,7 +7,7 @@ const apiData = require('../src/api-data');
 describe('apiData', () => {
   before(() => {
     global.apiData = {};
-    chai.spy.on(apiData, ['getBookingData', 'getUserData', 'getRoomData'], () => {})
+    chai.spy.on(apiData, ['getBookingData', 'getUserData', 'getRoomData', 'postNewBooking'], () => {})
   })
 
   it('should be able to call getBookingData once', () => {
@@ -38,5 +38,16 @@ describe('apiData', () => {
   it('should not be able to call getRoomData with any arguments', () => {
     apiData.getRoomData();
     expect(apiData.getRoomData).to.have.been.called.with();
+  })
+
+  it('should be able to call postNewBooking once', () => {
+    apiData.postNewBooking();
+    expect(apiData.postNewBooking).to.have.been.called(1);
+  })
+
+  it('should be call postNewBooking with a formatted booking', () => {
+    let booking = { userID: 1, date: "2020/11/29", roomNumber: 24 };
+    apiData.postNewBooking(booking);
+    expect(apiData.postNewBooking).to.have.been.called.with(booking);
   })
 })
